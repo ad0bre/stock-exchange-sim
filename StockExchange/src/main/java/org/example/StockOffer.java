33ea2c2b0963118b6c800afaef4c24b27ed3a33c
Andrei Dobre <andrei.dobr3@gmail.com>
2024-10-30 00:27:56 +0200

    Created Client class

--- StockExchange/src/main/java/org/example/StockOffer.java
+++ StockExchange/src/main/java/org/example/StockOffer.java
@@ -1,16 +1,25 @@
 package org.example;
 
 import java.util.Objects;
+import java.util.UUID;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class StockOffer {
+    private UUID id;
     private String type;
     private int shares;
     private double pricePerUnit;
+    private boolean isOffer;
 
-    public StockOffer(String type, double price, int shares) {
+    private final Lock lock = new ReentrantLock();
+
+    public StockOffer(String type, double price, int shares, boolean isOffer) {
+        this.id = UUID.randomUUID();
         this.type = type;
         this.pricePerUnit = price;
         this.shares = shares;
+        this.isOffer = isOffer;
     }
 
     public String getType() {
@@ -18,23 +27,23 @@ public class StockOffer {
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o){
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        StockOffer stockUnit = (StockOffer) o;
-        return Objects.equals(this.type, stockUnit.type);
+        StockOffer that = (StockOffer) o;
+        return this.id.equals(that.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(type);
+        return Objects.hash(id);
     }
 
     public double getPrice() {
         return pricePerUnit;
     }
 
-    public boolean isOffer(StockOffer request){
-        return this.type.equals(request.getType()) && this.pricePerUnit <= request.getPrice();
+    public boolean match(StockOffer request){
+        return this.type.equals(request.getType()) && this.pricePerUnit == request.getPrice();
     }
 }
